0823f18a5f2812a4ef2436ab3230956fbc127814
tamasKiss <sauriaster@gmail.com>
2018-03-02 22:15:40 +0100

    testing main + Glimmier is the best pony

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include <SFML/Window.hpp>
+
 int main(int argc, char** argv) {
-    std::cout << "Hello, world!" << std::endl;
+    sf::Window w(sf::VideoMode(800, 600), "Starlight Glimmer is still Best Pony");
+
+    while(w.isOpen()) {
+        sf::Event evt;
+        while(w.pollEvent(evt)) {
+            if(evt.type == sf::Event::Closed) {
+                w.close();
+            }
+        }
+    }
     return 0;
 }
