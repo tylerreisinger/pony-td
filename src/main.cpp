ac0be1ead67f3817aa01abbc0ab4e03fc2d43036
Tamas Kiss <sauriaster@gmail.com>
2018-03-05 21:41:29 +0100

    refractored main with test drawing out

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,67 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+
+void check_if_there_is_no_event(sf::RenderWindow& w) {
+    sf::Event evt;
+    while(w.pollEvent(evt)) {
+        if(evt.type == sf::Event::Closed) {
+            w.close();
+        }
+    }
+}
+
+void check_font_file_path(std::ifstream& font_file) {
+    if(!font_file.is_open()) {
+        std::cerr << "Failed to load font file." << std::endl;
+        throw std::runtime_error("Font file not found.");
+    }
+}
+
+std::vector<char> load_font_data(const std::string& file_path) {
+    std::ifstream font_file(file_path);
+    check_font_file_path(font_file);
+    font_file.seekg(0, std::ios_base::end);
+    std::size_t font_size = font_file.tellg();
+    font_file.seekg(0, std::ios_base::beg);
+    auto data = std::vector<char>(font_size + 1);
+    font_file.read(data.data(), font_size);
+    return data;
+}
+
+sf::Font create_font_from_data(const std::vector<char>& data) {
+    sf::Font font;
+    bool result = font.loadFromMemory(data.data(), data.size());
+    if(!result) {
+        throw std::invalid_argument("invalid font data supplied");
+    }
+    return font;
+}
+
+sf::Text make_message(const std::string& m, const sf::Font& font) {
+    sf::Text message(m, font);
+    message.setCharacterSize(12);
+    message.setColor(sf::Color(255, 0, 255));
+    return message;
+}
+
+
 int main(int argc, char** argv) {
-    sf::Window w(sf::VideoMode(800, 600), "Starlight Glimmer is still Best Pony");
+    sf::RenderWindow w(
+            sf::VideoMode(800, 600), "Starlight Glimmer is still Best Pony");
+
+    const std::string file_path = "Assets/Fonts/Inconsolata/Inconsolata.otf";
+
+    std::vector<char> chars = load_font_data(file_path);
+    sf::Font font = create_font_from_data(chars);
 
     while(w.isOpen()) {
+        check_if_there_is_no_event(w);
+        w.clear();
 
-        sf::Event evt;
-        while(w.pollEvent(evt)) {
-            if(evt.type == sf::Event::Closed) {
-                w.close();
-            }
-        }
+        w.draw(make_message("Glimmer is the best pone", font));
 
-        std::ifstream font_file("Assets/Fonts/Inconsolata/Inconsolata.otf");
-
-        if(!font_file.is_open()) {
-            std::cout << "Failed to load font file." << std::endl;
-            return -1;
-        }
-        font_file.seekg(0, std::ios_base::end);
-        std::size_t font_size = font_file.tellg();
-        font_file.seekg(0, std::ios_base::beg);
-        std::cout << font_size << std::endl;
-        std::vector<char> data(font_size+1);
-        font_file.read(data.data(), font_size);
-
-        //Put drawing stuff here
-        sf::Font font;
-        auto my_font = font.loadFromMemory(data.data(), font_size);
-        std::cout << my_font << std::endl;
-        sf::Text message("Hello Glimmer", font);
-
-        w.close();
+        w.display();
     }
     return 0;
 }
